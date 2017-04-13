8c8bee88638045160360194c1a2abe47aa3471d9
starlkj <starlkj@exntu.com>
2017-04-13 13:13:10 +0900

    변경

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,3 @@
-FROM mysql:5.6
-ENV REPLICATION_USER exntu
-ENV REPLICATION_PASSWORD t~paas
-ENV SERVER_ID 1
-ENV DB_NAME exntu
-COPY replication-entrypoint.sh /usr/local/bin/replication-entrypoint.sh
+FROM mariadb:10.1
+COPY replication-entrypoint.sh /replication-entrypoint.sh
 ENTRYPOINT ["replication-entrypoint.sh"]
-CMD ["mysqld"]
--- replication-entrypoint.sh
+++ replication-entrypoint.sh
@@ -1,70 +1,31 @@
 #!/bin/bash
-set -eo pipefail
 
 cat > /etc/mysql/my.cnf << EOF
-[client]
-port            = 3306
-socket          = /var/run/mysqld/mysqld.sock
-default-character-set = utf8
+#
+# Galera Cluster: mandatory settings
+#
 
-[mysqld_safe]
-pid-file        = /var/run/mysqld/mysqld.pid
-socket          = /var/run/mysqld/mysqld.sock
-nice            = 0
+[server]
+bind-address=0.0.0.0
+binlog_format=row
+default_storage_engine=InnoDB
+innodb_autoinc_lock_mode=2
+innodb_locks_unsafe_for_binlog=1
+query_cache_size=0
+query_cache_type=0
 
-[mysqld]
-user            = mysql
-pid-file        = /var/run/mysqld/mysqld.pid
-socket          = /var/run/mysqld/mysqld.sock
-port            = 3306
-basedir         = /usr
-datadir         = /var/lib/mysql
-tmpdir          = /tmp
-lc-messages-dir = /usr/share/mysql
-explicit_defaults_for_timestamp
-init_connect    = "SET collation_connection = utf8_general_ci"
-init_connect    = "SET NAMES utf8"
-character-set-server = utf8
-collation-server = utf8_general_ci
+[galera]
+wsrep_on=ON
+wsrep_provider="/usr/lib/galera/libgalera_smm.so"
+wsrep_cluster_address="gcomm://mariadb-node-0,mariadb-node-1,mariadb-node-2"
+wsrep-sst-method=rsync
 
-server-id      = $SERVER_ID
-log_bin        = /var/log/mysql/mysql-bin.log
-binlog_do_db   = $DB_NAME
-sql_mode=NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES
-symbolic-links=0
-!includedir /etc/mysql/conf.d/
+#
+# Optional setting
+#
 
+# Tune this value for your system, roughly 2x cores; see https://mariadb.com/kb/en/mariadb/galera-cluster-system-variables/#wsrep_slave_threads
+# wsrep_slave_threads=1
 
-# performance setting
-#join_buffer_size        = 192K
-#tmp_table_size          = 16M
-#max_heap_table_size     = 16M
-#innodb_buffer_pool_size = 4G
-#query_cache_type        = 0
-
-[mysql]
-default-character-set = utf8
-
-[mysqldump]
-default-character-set = utf8
+# innodb_flush_log_at_trx_commit=0
 EOF
-
-cat >/docker-entrypoint-initdb.d/init-master.sh  <<'EOF'
-#!/bin/bash
-echo Creating replication user ...
-mysql -u root -e "\
-  GRANT \
-    FILE, \
-    SELECT, \
-    SHOW VIEW, \
-    LOCK TABLES, \
-    RELOAD, \
-    REPLICATION SLAVE, \
-    REPLICATION CLIENT \
-  ON *.* \
-  TO '$REPLICATION_USER'@'%' \
-  IDENTIFIED BY '$REPLICATION_PASSWORD'; \
-  FLUSH PRIVILEGES; \
-"
-
-exec docker-entrypoint.sh "$@"
